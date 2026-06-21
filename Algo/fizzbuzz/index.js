2c4aaa4362edbbfee5f8fe5bf86f0d2434e9c09d
Kais zouaoui <kais.zouaoui@fulll.fr>
2026-06-21 16:26:05 +0200

    update code

--- Algo/fizzbuzz/index.js
+++ Algo/fizzbuzz/index.js
@@ -1,9 +1,9 @@
 const { fizzbuzz } = require('./fizzbuzz');
 
-const n = Number(process.argv[2] ?? 100);
+const n = Number(process.argv[2] ?? 100); // default value is 100
 
 if (!Number.isInteger(n) || n < 1) {
-  console.error('Usage: npm start -- <positive integer>');
+  console.error('shoud be a positive integer');
   process.exit(1);
 }
 
